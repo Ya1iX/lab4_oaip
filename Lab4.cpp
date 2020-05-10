44c4362404805f9d271cabf5ad7ba1cb4272b3bd
Ya1iX <62989500+Ya1iX@users.noreply.github.com>
2020-05-11 00:47:14 +0300

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -9,7 +9,9 @@ int main()
 {
 	CTree* tree = new CTree("in.txt", "tree");
 	tree->view_all();
+	
 	printf("Nearest key to %f is %i", tree->average(), tree->findNearest());
+	
 	delete tree;
 	tree = NULL;
 }
